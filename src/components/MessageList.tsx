0b20fc4a0dc8e5ea5a2f9c53f8aed4cb9194e09f
watcharapun <watcharapun@thinknet.co.th>
2025-05-20 23:14:52 +1000

    fix MessageList add list

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -21,10 +21,13 @@ export default function MessageList({ userId }: { userId: string }) {
     <div className="mt-6">
       <h2 className="text-xl font-semibold mb-2">Your Messages</h2>
       <ul className="space-y-2">
-        {messages.sort((a, b) => a.createdAt - b.createdAt).map((msg, i) => {
+        {messages.sort((b, a) => a.createdAt - b.createdAt).map((msg, i) => {
+          console.log(messages)
           return (
             <li key={i} className="bg-gray-100 p-3 rounded text-black">
-              <strong>{msg.name}</strong>: {msg.message}
+              <strong>Name</strong>: {msg.name} <br />
+              <strong>Message</strong>: {msg.message} <br />
+              <strong>User ID</strong>: {msg.userId}
             </li>
           )
         })}
